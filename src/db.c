d49ee6b498aed3b36425f671240efc9a3e7e720e
Andreas Ellsén <andreas.ellsen@meriq.com>
2014-07-20 03:03:32 +0100

    Increase string size to prevent runtime faults

--- src/db.c
+++ src/db.c
@@ -125,7 +125,7 @@ void mprog_read_programs args( ( FILE * fp, MOB_INDEX_DATA * pMobIndex ) );
  * Increase MAX_STRING if you have too.
  * Tune the others only if you understand what you're doing.
  */
-#define			MAX_STRING	1048576
+#define			MAX_STRING	2048576
 #define			MAX_PERM_BLOCK	131072
 #define			MAX_MEM_LIST	11
 
